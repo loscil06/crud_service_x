ede14326401e5908a1d276828fd8f8f6fed4eabd
Alan Ramirez <loscil94@hotmail.com>
2018-10-10 15:19:05 -0500

    Added more functions to main lib.

--- lib/common.php
+++ lib/common.php
@@ -14,11 +14,39 @@ function connectDblocal ()
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $pdo;
 }
-function prepareAndExecute ($sql, $pdo)
+function associateKeyValueInSQLStatement ($array, $sql)
 {
-	$query = $pdo->prepare($sql);
-	$query->execute();
-	return $query;
+	foreach ($array as $key => $value) {
+		$sql .= "`$key` = '$value', ";
+	}
+	$sql = substr($sql, 0, -2);
+	return $sql;
+}
+function createArrayParameters ($tablename)
+{
+	$sql = "SELECT `COLUMN_NAME` 
+		FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+		WHERE `TABLE_SCHEMA`='service_x_employees' 
+		    AND `TABLE_NAME`='$tablename';";
+	$result = queryBindExecute($this->pdo, $sql, $null);
+	while ($row = $result->fetch()) {
+		$parameters[] = array('$row' => ":$row");
+	}
+	return $parameters;
+}
+function queryBindExecute ($pdo, $sql, $parameters)
+{
+	//Theres values to be bound
+	if (!empty($parameters)) {
+		$query = $pdo->prepare($sql);
+		foreach ($parameters as $key => $value) {
+			
+		}
+	} else {//simple query with no values to be bound
+		$query = $pdo->prepare($sql);
+		$query->execute();
+		return $query;
+	}
 }
 //TODO: Implement this
 class AdminLogin
@@ -44,12 +72,28 @@ class GetEmployees
 	}
 	public function getEmployees()
 	{
+		//if implementing limits and pagination, replace LIMIT DEFAULT when needed
 		$sql = file_get_contents(__DIR__ . '/../lib/select.all.employees.sql');
 		$sql .= "LIMIT $this->limit['min'], $this->limit['max']";
-		return prepareAndExecute($sql, $this->pdo);
+		return queryBindExecute($sql, $this->pdo, $parameters);
 	}
 }
-class UpdateEmployee
+class UpdateInformation
 {
-
+	private $id;
+	private $pdo;
+	private $tablename;
+	function __construct(string $tablename, string $id, PDO $pdo)
+	{
+		$this->id = $id;
+		$this->pdo = $pdo;
+		$this->tablename = $tablename;
+	}
+	public function syncInfo ()
+	{
+		$sql = "UPDATE `service_x_employees`.`$tablename` SET";
+		$parameters = createArrayParameters($tablename);
+		$sql = associateKeyValueInSQLStatement($parameters, $sql);
+		$sql .= "WHERE `id` = $this->id";
+	}
 }
